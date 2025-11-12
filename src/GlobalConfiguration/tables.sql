5568f1eb96e03b64d04be6208ee61f5fa8b5ef52
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2025-11-12 06:18:53 +0000

    refactor: correct table definitions and constraints in tables.sql

--- src/GlobalConfiguration/tables.sql
+++ src/GlobalConfiguration/tables.sql
@@ -2,8 +2,34 @@ create database QuanLyPhongKhachSan;
 
 use QuanLyPhongKhachSan;
 
-create table Phong(
-  ma         char(10)        not null,
+create table KhachHang(
+  MaKH        char(10)       not null,
+  HoVaTen     nvarchar(128)  not null,
+  Phai        char(3)        not null,
+  SDT         char(10)       not null,
 
-  constraint PK_Phong primary key (ma)
+  constraint PK_KhachHang primary key (MaKh),
+  constraint CK_KhachHang_Phai check (Phai = 'nam' or Phai = 'nu')  
+);
+
+create table Phong(
+  MaPhong  char(10) not null,
+  SoGiuong  int not null,
+  GiaTien float not null,
+  DangDat bit  not null,
+  
+  constraint PK_Phong primary key (MaPhong),
+  constraint CK_Phong_SoGiuong check (SoGiuong > 0)
+);
+
+create table HoaDon(
+  MaKH        char(10)       not null,
+  MaPhong     char(10)       not null,
+  NgayNhan    smalldatetime  not null,
+  NgayTra     smalldatetime  not null,
+  ThanhGia float not null,
+  DaThanhToan BIT not null,
+
+  constraint PK_HoaDon primary key (MaPhong, NgayNhan),
+  constraint CK_HoaDon_NgayNhan check(DATEDIFF(NgayNhan, NgayTra) > 0)
 );
